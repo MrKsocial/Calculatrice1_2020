1878733b273d3874cfdbb5dd7fcfd3d4cbf7cfec
MrKsocial <josh0824@hotmail.com>
2020-09-19 17:15:28 +0200

    Ajoutez des fichiers projet.

---
+++ Calculatrice1_2020/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatrice1_2020
+{
+    public partial class Form1 : Form
+    {
+
+        String operation = "";
+        double valeur = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            Button button=(Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = textBox1.Text + "0";
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }*/
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="0")
+            {
+                textBox1.Clear();
+                Button button = (Button)sender;
+                textBox1.Text = textBox1.Text + button.Text;
+            }
+            else
+            {
+                Button button = (Button)sender;
+                textBox1.Text = textBox1.Text + button.Text;
+            }
+            
+        }
+
+        private void op_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            if(textBox1.Text == "")
+            {
+                textBox1.Clear();
+            }
+            else
+            {
+                valeur = double.Parse(textBox1.Text);
+                textBox1.Text = "0";
+            }
+            
+        }
+
+        private void calcul_Click(object sender, EventArgs e)
+        {
+            switch(operation)
+            {
+                case "+":
+                    textBox1.Text = (valeur + Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "X":
+                    textBox1.Text = (valeur * Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                    textBox1.Text = (valeur / Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (valeur - Double.Parse(textBox1.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            
+            textBox1.Clear();
+            operation = "";
+            valeur = 0;
+            
+
+        }
+    }
+}
---
+++ calculatriceConsole/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace calculatriceConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Double soluce = 0;
+        start:
+            Double nbre=0;
+            Double nbre2 = 0;
+            
+            String operation = "";
+            
+
+            if (soluce == 0)
+            {
+                Console.WriteLine("Ecrivez un nombre.");
+                nbre = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ecrivez un opérateur de calcul.");
+                operation = Console.ReadLine();
+
+                Console.WriteLine("Ecrivez un nombre.");
+                nbre2 = double.Parse(Console.ReadLine());
+            }
+
+            else if(soluce !=0)
+            {
+                nbre = soluce;
+                nbre2 = 0;
+                Console.WriteLine("Votre solution "+soluce+" Ecrivez un opérateur de calcul.");
+                operation = Console.ReadLine();
+
+                Console.WriteLine("Ecrivez un nombre.");
+                nbre2 = double.Parse(Console.ReadLine());
+            }
+            
+
+            switch (operation)
+            {
+                case "+":
+                    soluce=nbre + nbre2;
+                    nbre = 0;
+                    nbre2 = 0;
+                    operation = "";
+                    Console.WriteLine("Votre solution : " + soluce);
+                    goto start;
+                    
+                case "-":
+                    soluce = nbre - nbre2;
+                    nbre = 0;
+                    nbre2 = 0;
+                    operation = "";
+                    Console.WriteLine("Votre solution : " + soluce);
+                    goto start;
+                case "*":
+                    soluce = nbre * nbre2;
+                    nbre = 0;
+                    nbre2 = 0;
+                    operation = "";
+                    Console.WriteLine("Votre solution : " + soluce);
+                    goto start;
+                case "/":
+                    soluce = nbre / nbre2;
+                    nbre = 0;
+                    nbre2 = 0;
+                    operation = "";
+                    Console.WriteLine("Votre solution : " + soluce);
+                    goto start;
+                default:
+                    nbre = 0;
+                    nbre2 = 0;
+                    operation = "";
+                    goto start;
+                    //break;
+                    
+
+            }
+        }
+    }
+}
